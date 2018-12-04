148e1cdb6db334278c8792b18246b4ef8c943146
Moltenfire <moltenfires@googlemail.com>
2018-12-04 17:29:22 +0000

    Day 4

--- 4a.py
+++ 4a.py
@@ -2,35 +2,32 @@ import collections
 import itertools
 import helper
 import re
-import operator
 
 values = helper.load_in_list('input_4.txt', str)
 values.sort()
 
-guards = collections.defaultdict(int)
-guard2 = collections.defaultdict(list)
-curent_guard = None
-s = None
+sleep_time = collections.defaultdict(int)
+mins_sleep = collections.defaultdict(list)
+g_id = None
+start = None
 for v in values:
     x = list(map(int, re.findall('\d+', v)))
-    l = v[19]
-    if l == "G":
-        curent_guard = x[-1]
-    elif l == "f":
-        s = x
-    elif l == "w":
-        start = s[4]
+    if "Guard" in v:
+        g_id = x[-1]
+    elif "falls" in v:
+        start = x[4]
+    elif "wakes" in v:
         end = x[4]
         time = end - start - 1
-        guards[curent_guard] += time
-        guard2[curent_guard] += list(range(start, end))
+        sleep_time[g_id] += time
+        mins_sleep[g_id] += list(range(start, end))
 
-g_id = max(guards.items(), key=operator.itemgetter(1))[0]
-minute = collections.Counter(guard2[g_id]).most_common(1)[0][0]
-# print(g_id)
-# print(minute)
-# print(g_id * minute)
+mins_sleep_counter = {g_id : collections.Counter(mins_sleep[g_id]).most_common(1)[0] for g_id in mins_sleep}
 
-for g in guard2:
-    minute = collections.Counter(guard2[g]).most_common(1)[0]
-    print(g, minute, g * minute[0])
+g_id = max(sleep_time.items(), key=lambda x : x[1])[0]
+minute = mins_sleep_counter[g_id][0]
+print(g_id * minute)
+
+g_id = max(mins_sleep_counter.items(), key=lambda x : x[1][1])[0]
+minute = mins_sleep_counter[g_id][0]
+print(g_id * minute)
