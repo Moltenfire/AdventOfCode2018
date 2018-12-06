78ce797860ed7a9f354e917e303ecd52cf70f038
Moltenfire <moltenfires@googlemail.com>
2018-12-06 17:57:47 +0000

    Day 6 all in one

--- 6a.py
+++ 6a.py
@@ -9,39 +9,29 @@ min_x = min(coords.keys(), key=lambda x : x[0])[0]
 max_x = max(coords.keys(), key=lambda x : x[0])[0]
 min_y = min(coords.keys(), key=lambda x : x[1])[1]
 max_y = max(coords.keys(), key=lambda x : x[1])[1]
-print(min_x, min_y, max_x, max_y)
+
+max_dist = 10000
 
 def dist(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
 l = []
-for y in range(min_y, max_y):
-    for x in range(min_x, max_x):
+edge = set()
+total = 0
+for y in range(min_y, max_y+1):
+    for x in range(min_x, max_x+1):
         k = (x,y)
         dists = [dist(c, k) for c in coords]
         min_d = min(dists)
+        # Part 1
         if dists.count(min_d) == 1:
             l.append(dists.index(min_d))
+            if x == min_x or x == max_x or y == min_y or y == max_y:
+                edge.add(dists.index(min_d))
+        # Part 2
+        if sum(dists) < max_dist:
+            total += 1
 
-r = []
-for y in range(min_y - 1, max_y + 1):
-    for x in [min_x - 1, max_x + 1]:
-        k = (x,y)
-        dists = [dist(c, k) for c in coords]
-        min_d = min(dists)
-        if dists.count(min_d) == 1:
-            r.append(dists.index(min_d))
-
-for x in range(min_x - 1, max_x + 1):
-    for y in [min_y - 1, max_y + 1]:
-        k = (x,y)
-        dists = [dist(c, k) for c in coords]
-        min_d = min(dists)
-        if dists.count(min_d) == 1:
-            r.append(dists.index(min_d))
-
-r = set(r)
-
-for i, count in collections.Counter(l).most_common():
-    if i not in r:
-        print(i, count)
+area = [count for i, count in collections.Counter(l).most_common() if i not in edge][0]
+print(area)
+print(total)
