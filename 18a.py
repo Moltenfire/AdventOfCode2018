44e1d74ec1e9cae5654eabcb8fb1b359c80907ee
Moltenfire <moltenfires@googlemail.com>
2018-12-18 18:00:40 +0000

    Day 18 numpy optimisations

--- 18a.py
+++ 18a.py
@@ -2,7 +2,7 @@ import collections
 import itertools
 import helper
 import copy
-import re
+import numpy as np
 
 def print_grid(grid):
     print()
@@ -10,42 +10,26 @@ def print_grid(grid):
         print(''.join(c for x, c in enumerate(grid[:,y])))
 
 def tick(g):
-    g2 = copy.deepcopy(g)
+    g2 = g.copy()
     max_x = g.shape[0]
     max_y = g.shape[1]
 
     for i in range(max_x):
         for j in range(max_y):
-            space = 0
-            trees = 0
-            lumber = 0
-            for x in range(i-1, i+2):
-                
-                if x < 0 or x > max_x-1:
-                    continue
-                for y in range(j-1, j+2):
-                    if y < 0 or y > max_y-1:
-                        continue
-                    if x == i and y == j:
-                        continue
-                    c = g[x,y]
-                    if c == '.':
-                        space += 1
-                    elif c == '|':
-                        trees += 1
-                    else:
-                        lumber += 1
-                
-            # print(i,j, space, trees, lumber, g[i,j])
+            s = g[max(0, i-1):i+2, max(0, j-1):j+2]
             c = g[i,j]
             if c == '.':
+                trees = np.count_nonzero(s == '|')
                 if trees >= 3:
                     g2[i,j] = '|'
             elif c == '|':
+                lumber = np.count_nonzero(s == '#')
                 if lumber >= 3:
                     g2[i,j] = '#'
             else:
-                if trees == 0 or lumber == 0:
+                trees = np.count_nonzero(s == '|')
+                lumber = np.count_nonzero(s == '#')
+                if trees == 0 or lumber == 1:
                     g2[i,j] = '.'
 
     return g2
@@ -82,7 +66,7 @@ for i in range(1000000000):
     else:
         if t == start:
             r = i - last_i
-            m = ((1000000000 - last_i) % r) - 1
+            m = ((1000000000 - last_i - 1) % r)
             print(scores[m])
             break
         else:
